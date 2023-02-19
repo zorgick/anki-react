f7035a025528ff2ceaf0d6f7884e175c648ef032
zorgick <zorgizorgik@gmail.com>
2023-02-19 00:49:18 +0000

    Update 4 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import WithLag from "./WithLag";
+import WithoutLag from "./WithoutLag";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div>
+        <h4>Scrolling with lag</h4>
+        <WithLag />
+      </div>
+      <div>
+        <h4>Scrolling without lag</h4>
+        <WithoutLag />
+      </div>
+    </div>
+  );
 }
 
 export default App;
