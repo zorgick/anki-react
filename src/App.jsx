b3427389f916b3af101c45dd926c079961196bc2
Zackary Kononov <zorgizorgik@gmail.com>
2023-02-20 22:06:34 +0300

    react 18 batch update same behaviour

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-import "./App.css";
+import { useState, useLayoutEffect } from "react";
 
-function App() {
-  return <div className="App"></div>;
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  function handleClick() {
+    console.log("=== click ===");
+    // Will the both be batched?
+    setCount((c) => c + 1);
+    setFlag((f) => !f);
+    fetchSomething().then(() => {
+      console.log("=== click promise ===");
+      // Will the both be batched?
+      setCount((c) => c + 1);
+      setFlag((f) => !f);
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Next</button>
+      <h1 style={{ color: flag ? "blue" : "black" }}>{count}</h1>
+      <LogEvents />
+    </div>
+  );
 }
 
-export default App;
+function LogEvents(props) {
+  useLayoutEffect(() => {
+    console.log("Commit");
+  });
+  console.log("Render");
+  return null;
+}
+
+function fetchSomething() {
+  return new Promise((resolve) => setTimeout(resolve, 100));
+}
