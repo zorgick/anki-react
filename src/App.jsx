17a844c62680ab2571e75707b0cbb8bd35735c17
Zackary Kononov <zorgizorgik@gmail.com>
2023-03-02 22:38:34 +0300

    isolated hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return <div className="App"></div>;
+function UserCount() {
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState("John");
+
+  function handleClick() {
+    setCount(count + 1);
+    setUser(count % 2 ? "John" : "Janet");
+  }
+
+  function handleUserClick() {
+    setUser(user == "John" ? "Janet" : "John");
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <button onClick={handleUserClick}>
+        Next user
+      </button>
+      <h3>  
+        {count} {user}
+      </h3>
+    </>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <UserCount />
+      <UserCount />
+    </>
+  );
+}
